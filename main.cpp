b138147bf169c4c87cb83d9fff49311ceff83d04
nebraszka <majusxp@gmail.com>
2023-05-22 00:15:06 +0200

    Refactor code and add cells colors

--- main.cpp
+++ main.cpp
@@ -1,51 +1,44 @@
-// Include the necessary libraries
 #include <GLFW/glfw3.h>
 #include <cstdlib>
 #include <ctime>
 #include <vector>
+#include <cstring>
 
 #define CELLS_NUM 10000
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 800
-#define GRID_SIZE 100
+#define GRID_SIZE 500
 
 struct Position {
     float x;
     float y;
 };
 
-// Not implemented yet
-struct Velocity {
-    float xVelocity;
-    float yVelocity;
+struct Color {
+    float r;
+    float g;
+    float b;
 };
 
 struct Cell {
-    int color;
-    struct Position position;
+    Color color;
+    Position position;
+    bool alive;
 };
 
-// Define the grid of cells
-bool grid[GRID_SIZE][GRID_SIZE] = {false};
+Cell grid[GRID_SIZE][GRID_SIZE] = {{{0}}};
 
-// Function to draw the grid
 void DrawGrid() {
-    // Clear the screen
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-
-    // Set the color to green
-    glColor3f(0.0f, 1.0f, 0.0f);
-
-    // Draw the cells
     glBegin(GL_QUADS);
     for (int x = 0; x < GRID_SIZE; ++x) {
         for (int y = 0; y < GRID_SIZE; ++y) {
-            if (grid[x][y]) {
-                // Change coordinates from grid coordinates to OpenGL coordinates [-1, 1]
+            if (grid[x][y].alive) {
                 float xPos = (float)x / GRID_SIZE * 2 - 1;
                 float yPos = (float)y / GRID_SIZE * 2 - 1;
                 float cellSize = 2.0f / GRID_SIZE;
+                glColor3f(grid[x][y].color.r, grid[x][y].color.g, grid[x][y].color.b);
                 glVertex2f(xPos, yPos);
                 glVertex2f(xPos + cellSize, yPos);
                 glVertex2f(xPos + cellSize, yPos + cellSize);
@@ -54,10 +47,6 @@ void DrawGrid() {
         }
     }
     glEnd();
-}
-
-bool isCellAlive(int x, int y) {
-    return grid[x][y];
 }
 
 bool decideIfAlive(int x, int y) {
@@ -69,78 +58,90 @@ bool decideIfAlive(int x, int y) {
             }
             int newX = (x + dx + GRID_SIZE) % GRID_SIZE;
             int newY = (y + dy + GRID_SIZE) % GRID_SIZE;
-            if (isCellAlive(newX, newY)) {
+            if (grid[newX][newY].alive) {
                 ++aliveNeighbors;
             }
         }
     }
-    if (isCellAlive(x, y)) {
-        return aliveNeighbors == 2 || aliveNeighbors == 3;
-    } else {
-        return aliveNeighbors == 3;
-    }
+    return grid[x][y].alive ? (aliveNeighbors == 2 || aliveNeighbors == 3) : (aliveNeighbors == 3);
 }
 
-// Function to update the positions of the cells
-void UpdateGrid() {
-    // Copy the current grid
-    bool newGrid[GRID_SIZE][GRID_SIZE] = {false};
-    for (int x = 0; x < GRID_SIZE; ++x) {
-        for (int y = 0; y < GRID_SIZE; ++y) {
-            newGrid[x][y] = decideIfAlive(x, y);
+Color inheritColor(int x, int y) {
+    float r = 0.0f;
+    float g = 0.0f;
+    float b = 0.0f;
+    int aliveNeighbours = 0;
+
+    for (int dx = -1; dx <= 1; ++dx) {
+        for (int dy = -1; dy <= 1; ++dy) {
+            if (dx == 0 && dy == 0) {
+                continue;
+            }
+            int newX = (x + dx + GRID_SIZE) % GRID_SIZE;
+            int newY = (y + dy + GRID_SIZE) % GRID_SIZE;
+            if (grid[newX][newY].alive) {
+                r += grid[newX][newY].color.r;
+                g += grid[newX][newY].color.g;
+                b += grid[newX][newY].color.b;
+                aliveNeighbours++;
+            }
         }
     }
-    // Replace the old grid with the new grid
-    for (int x = 0; x < GRID_SIZE; ++x) {
-        for (int y = 0; y < GRID_SIZE; ++y) {
-            grid[x][y] = newGrid[x][y];
-        }
+    if (aliveNeighbours == 0) {
+        return {0.0f, 0.0f, 0.0f};
     }
+
+    return {r / aliveNeighbours, g / aliveNeighbours, b / aliveNeighbours};
 }
+
+void UpdateGrid() {
+    Cell newGrid[GRID_SIZE][GRID_SIZE] = {{{0}}};
+    for (int x = 0; x < GRID_SIZE; ++x) {
+        for (int y = 0; y < GRID_SIZE; ++y) {
+            newGrid[x][y].alive = decideIfAlive(x, y);
+            if(newGrid[x][y].alive) {
+                // If the cell is alive in the next generation, inherit the color from its neighbors
+                newGrid[x][y].color = inheritColor(x, y);
+            }
+        }
+    }
+    memcpy(grid, newGrid, sizeof(newGrid));
+}
+
 
 int main() {
-    // Initialize the library
     if (!glfwInit()) {
         return -1;
     }
 
-    // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Cellular Automata", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "GoL Maja Nagarnowicz Agnieszka Stefankowska", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
     }
 
-    // Make the window's context current
     glfwMakeContextCurrent(window);
 
-    // Initialize the grid with some random cells
     srand(time(NULL));
     for (int i = 0; i < CELLS_NUM; ++i) {
         int x = rand() % GRID_SIZE;
         int y = rand() % GRID_SIZE;
-        grid[x][y] = true;
+        grid[x][y].alive = true;
+        // Set the color to some initial value
+        grid[x][y].color.r = (rand() % 255) / 255.0f;
+        grid[x][y].color.g = (rand() % 255) / 255.0f;
+        grid[x][y].color.b = (rand() % 255) / 255.0f;
     }
-
-    // Loop until the user closes the window
     while (!glfwWindowShouldClose(window)) {
-        // Get the current time
         static double lastTime = glfwGetTime();
         double currentTime = glfwGetTime();
-        // If one second has passed since the last update
-        if (currentTime - lastTime >= 0.9) {
-            // Update the grid
+        if (currentTime - lastTime >= 0.05) {
             UpdateGrid();
             lastTime = currentTime;
         }
 
-        // Draw the grid
         DrawGrid();
-
-        // Swap front and back buffers
         glfwSwapBuffers(window);
-
-        // Poll for and process events
         glfwPollEvents();
     }
 
